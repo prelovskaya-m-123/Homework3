c3d37498a69512fb7ec4429eaefc135dfc1ecef8
Вадим Мария <maraslanowa@yandex.ru>
2025-05-30 01:32:04 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(); }
+        int pages = 33000;
+        byte hours = 90;
+        short price = 20000;
+        long study = 200L;
+        float temperature = 78.48f;
+        double distance = 1.4567777;
+        System.out.println("Значение переменной pages с типом int равно " + pages);
+        System.out.println("Значение переменной hours с типом byte равно " + hours);
+        System.out.println("Значение переменной price с типом short равно " + price);
+        System.out.println("Значение переменной study с типом long равно " + study);
+        System.out.println("Значение переменной temperature с типом float равно " + temperature);
+        System.out.println("Значение переменной distance с типом double равно " + distance);
+    }
 }
